cf05c12a9205fce0e70a6705d71bd17284e59146
Asugra84 <115951862+Asugra84@users.noreply.github.com>
2024-05-14 15:09:03 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const headerText = document.querySelector('section div h1');
+const commandLines = [
+    '# ls',
+    '/home/Asugra',
+    '# rm -rf / --no-preserve-root',
+    '# '
+];
+
+let lineIndex = 0;
+let characterIndex = 0;
+
+function type() {
+    if (lineIndex < commandLines.length) {
+        if (characterIndex < commandLines[lineIndex].length) {
+            headerText.innerHTML += commandLines[lineIndex].charAt(characterIndex);
+            characterIndex++;
+            setTimeout(type, 100); // Délai entre chaque caractère (ajustez selon vos préférences)
+        } else {
+            if (lineIndex === commandLines.length - 1) {
+                setTimeout(blinkCursor, 500); // Démarre le clignotement du curseur après la dernière ligne
+            } else {
+                headerText.innerHTML += '<br>'; // Saut de ligne après chaque ligne sauf la dernière
+            }
+            lineIndex++;
+            characterIndex = 0;
+            setTimeout(type, 1000); // Délai entre chaque ligne (ajustez selon vos préférences)
+        }
+    }
+}
+
+function blinkCursor() {
+    const cursor = document.createElement('span');
+    cursor.id = 'cursor';
+    cursor.textContent = '_';
+    headerText.appendChild(cursor);
+
+    setInterval(() => {
+        cursor.style.visibility = cursor.style.visibility === 'hidden' ? 'visible' : 'hidden';
+    }, 500);
+}
+
+type();
